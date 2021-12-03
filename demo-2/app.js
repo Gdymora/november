d8aa6b78bca023cbdb0b5cbb2f16c6602a8537dc
Alexandr <alekspetrofint@gmail.com>
2021-12-03 11:59:44 +0200

    first

---
+++ demo-2/app.js
@@ -0,0 +1,11 @@
+/* const { stealRing, characters } = require('./characters.js')
+
+stealRing(characters, 'Frodo')
+
+for (const c of characters) {
+    console.log(c)
+} */
+/* const log = require('./characters.js')
+log(); */
+const { a } = require('./characters.js')
+//a=5;
---
+++ demo-2/characters.js
@@ -0,0 +1,17 @@
+/* let characters = [
+    { name: 'Frodo', hasRing: false },
+    { name: 'Bildo', hasRing: false }
+]
+function stealRing(characters, owner) {
+    return characters.map(c => {
+        if (c.name == owner) {
+            c.hasRing = true;
+        } else {
+            c.hasRing = false;
+        }
+    })
+}
+
+//module.exports = { characters, stealRing }
+module.exports = function log() { console.log('log') } */
+ a;
